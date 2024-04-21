eaa5ee7106e397d2b9b34c632b2841be7397fb71
root <root@DESKTOP-82M5BL7.localdomain>
2024-04-22 00:49:55 +0300

    test2

--- build.sh
+++ build.sh
@@ -1,3 +1,13 @@
+sudo apt install \
+    libxrandr-dev \
+    libxcursor-dev \
+    libudev-dev \
+    libfreetype-dev \
+    libopenal-dev \
+    libflac-dev \
+    libvorbis-dev \
+    libgl1-mesa-dev \
+    libegl1-mesa-dev
 mkdir -p build
 cd build
 cmake ..
